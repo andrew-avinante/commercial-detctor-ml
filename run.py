f09622c691660aa6a2d5bb4cad02dbde15f39608
Andrew Avinante <andrewavinante@gmail.com>
2023-03-11 18:04:40 -0500

    Updated model

--- run.py
+++ run.py
@@ -2,30 +2,41 @@ import numpy as np
 import tensorflow as tf
 import tensorflow_hub as hub
 
-def predict_video(video_file, model_file):
+def predict_video(video_file, audio_file, model_file):
     # Load video data from .npy file
-    video = np.load(video_file)
-    
+    video = np.array([np.load(video_file)])
+    audio = np.array([np.load(audio_file)])
+
+    # audio = audio.reshape(-1, 1)
+    # audio = np.hstack((audio, audio))
+
+    audio = audio.reshape((1, 192, 1))
+    print(f"Videos shape: {video.shape}")
+    print(f"Audio shape: {audio.shape}")
+
     # Load the model from .h5 file
     model = tf.keras.models.load_model(model_file, custom_objects={'KerasLayer':hub.KerasLayer})
     
     # Predict the label for each frame in the video
-    predictions = model.predict(video)
+    predictions = model.predict([video, audio], batch_size=1)
     
     return predictions
 
 if __name__ == '__main__':
     # Replace with the actual file paths
-    video_file = 'test_dir/processed_video_0.npy'
+    prefix = 'test'
+    num = '004'
+
+    video_file = f'{prefix}_dir/processed_video_{num}.npy'
+    audio_file = f'{prefix}_dir/processed_audio_{num}.npy'
     model_file = 'trained_model.h5'
-    video_features = np.load(video_file)
-    predictions = predict_video(video_file, model_file)
+    predictions = predict_video(video_file, audio_file, model_file)
 
     isFade = False
     count = 0
-    result = np.array(np.argmax(predictions, axis=1))
+    result = np.argmax(predictions, axis=2)
     print(result)
-    for i in result:
+    for i in result[0]:
         count += 1
         if i == 1 and isFade == False:
             start = count / 24
--- train.py
+++ train.py
@@ -1,54 +1,115 @@
 import glob
 import tensorflow as tf
+import random
 import numpy as np
 from keras.utils import to_categorical
 from sklearn.model_selection import train_test_split
+from tensorflow.keras.callbacks import EarlyStopping, LearningRateScheduler
+from tensorflow.keras.utils import Sequence
 
-def build_model(input_shape):
-    model = tf.keras.Sequential()
-    model.add(tf.keras.layers.LSTM(32, input_shape=input_shape, return_sequences=True))
-    model.add(tf.keras.layers.LSTM(32))
-    model.add(tf.keras.layers.Dense(2, activation='softmax'))
+
+class CustomDataGenerator(Sequence):
+    def __init__(self, video_files, audio_files, labels_files, batch_size, shuffle=True):
+        self.video_files = video_files
+        self.audio_files = audio_files
+        self.labels_files = labels_files
+        self.batch_size = batch_size
+        self.shuffle = shuffle
+        self.indexes = np.arange(len(video_files))
+        self.on_epoch_end()
+
+    def __len__(self):
+        return int(np.ceil(len(self.video_files) / self.batch_size))
+
+    def __getitem__(self, idx):
+        indexes = self.indexes[idx * self.batch_size:(idx + 1) * self.batch_size]
+        video_batch = np.array([np.load(self.video_files[k]) for k in indexes])
+        audio_batch = np.array([np.load(self.audio_files[k]) for k in indexes])
+        label_batch = np.array([np.load(self.labels_files[k]) for k in indexes])
+        label_batch = to_categorical(label_batch)
+
+        audio_batch = audio_batch.reshape((audio_batch.shape[0], 192, 1))
+
+        # print(f"Labels shape: {label_batch.shape}")
+        # print(f"Videos shape: {video_batch.shape}")
+        # print(f"Audio shape: {audio_batch.shape}")
+
+        return [video_batch, audio_batch], label_batch
+
+    def on_epoch_end(self):
+        if self.shuffle:
+            np.random.shuffle(self.indexes)
+
+
+def build_model():
+    video_input = tf.keras.layers.Input(shape=(192, 128 * 128))
+    audio_input = tf.keras.layers.Input(shape=(192, 1))
     
+    video = tf.keras.layers.LSTM(128, return_sequences=True, input_shape=(192, 128 * 128))(video_input)
+    video = tf.keras.layers.Dropout(0.2)(video)
+    video = tf.keras.layers.LSTM(128, return_sequences=True)(video)
+    video = tf.keras.layers.Dropout(0.2)(video)
+    video = tf.keras.layers.LSTM(128, return_sequences=True)(video)
+    video = tf.keras.layers.Dropout(0.2)(video)
+    video = tf.keras.layers.Dense(128, activation='relu')(video)
+
+
+    audio = tf.keras.layers.LSTM(128, return_sequences=True, input_shape=(192, 1))(audio_input)
+    audio = tf.keras.layers.Dropout(0.2)(audio)
+    audio = tf.keras.layers.LSTM(128, return_sequences=True)(audio)
+    audio = tf.keras.layers.Dropout(0.2)(audio)
+    audio = tf.keras.layers.LSTM(128, return_sequences=True)(audio)
+    audio = tf.keras.layers.Dropout(0.2)(audio)
+    audio = tf.keras.layers.Dense(128, activation='relu')(audio)
+    
+    merged = tf.keras.layers.concatenate([video, audio])
+    merged = tf.keras.layers.Dense(128, activation='relu')(merged)
+    merged = tf.keras.layers.Dropout(0.2)(merged)
+    output = tf.keras.layers.Dense(2, activation='softmax')(merged)
+    
+    model = tf.keras.Model(inputs=[video_input, audio_input], outputs=output)
     model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
     model.summary()
 
     return model
 
-# Search the training directory for the required files
-video_features_files = glob.glob('training_dir/processed_video*.npy')
-# audio_features_files = glob.glob('training_dir/audio_features*.npy')
-labels_files = glob.glob('training_dir/labels*.npy')
+def lr_scheduler(epoch, lr):
+    if epoch % 10 == 0 and epoch > 0:
+        lr = lr * 0.1
+    return lr
 
-# Load the video and audio features into arrays
-video_features = np.concatenate([np.load(f) for f in video_features_files])
-# audio_features = np.concatenate([np.load(f) for f in audio_features_files])
-
-# Load the labels into an array
-labels = np.concatenate([np.load(f) for f in labels_files])
-
-def train_model(video_data, labels):
+def train_model(video_data, audio_data, labels, batch_size):
     # Split the data into train and validation sets
-    train_data, val_data, train_labels, val_labels = train_test_split(video_data, labels, test_size=0.2)
+    train_video_files, val_video_files, train_audio_files, val_audio_files, train_labels_files, val_labels_files = train_test_split(video_data, audio_data, labels, test_size=0.2)
     
-    # Get the input shape for the first video
-    input_shape = train_data[0].shape
-    
+    train_generator = CustomDataGenerator(train_video_files, train_audio_files, train_labels_files, batch_size)
+    val_generator = CustomDataGenerator(val_video_files, val_audio_files, val_labels_files, batch_size)
+
     # Build the model
-    model = build_model(input_shape)
+    model = build_model()
+
+    early_stopping = EarlyStopping(monitor='loss', patience=5, restore_best_weights=True)
+
+    lr_callback = LearningRateScheduler(lr_scheduler)
     
     # Train the model on the train data
-    history = model.fit(train_data, train_labels, epochs=10, batch_size=32, validation_data=(val_data, val_labels))
-    
-    # Save the trained model
+    model.fit(
+        train_generator,
+        epochs=50,
+        steps_per_epoch=len(train_video_files) // batch_size,
+        validation_data=val_generator,
+        validation_steps=len(val_video_files) // batch_size,
+        callbacks=[early_stopping, lr_callback]
+    )
+
     model.save('trained_model.h5')
     
     return model
 
+video_features_files = glob.glob('training_dir/processed_video*.npy')
+audio_features_files = glob.glob('training_dir/processed_audio*.npy')
+labels_files = glob.glob('training_dir/labels*.npy')
+
 # Example usage
-labels = to_categorical(labels)
 
-print(f"Labels shape: {labels.shape}")
-print(f"Videos shape: {video_features.shape}")
-
-model = train_model(video_features, labels)
+model = train_model(video_features_files, audio_features_files, labels_files, 32)
